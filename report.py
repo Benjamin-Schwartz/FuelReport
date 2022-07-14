3079f79383ea1fdc41ec383ef8927651ed0bcc9b
Benjamin-Schwartz <benjaminschwartz316@gmail.com>
2022-07-14 17:26:19 -0500

    finally got blackout working and AIshas table

--- report.py
+++ report.py
@@ -1,3 +1,4 @@
+import string
 import pandas as pd
 import openpyxl
 import sys
@@ -6,13 +7,15 @@ from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.engine import URL
 from sqlalchemy import create_engine
 from pandasql import sqldf
+import numpy as np
 
+mode = "AtHome"
 def connect():
     DRIVER_NAME = 'SQL Server'
     SERVER_NAME = 'SQLDATA'
     DATABASE_NAME = 'Rehab'
 
-       
+    
     #Connecting paramaters
     connection_string = f"""
             DRIVER={{{DRIVER_NAME}}};
@@ -33,8 +36,8 @@ def connect():
 report = pd.DataFrame(columns = ['Branch', 'Area', 'Region', 
                             'Lease_Id', 'Year', 'Make', 
                             'Model', 'VIN', 'Plate_Number',
-                            'Full_Name', 'Employee_Number', 'Role',
-                            'Azuga Device','Black Out' , 'Covered', 
+                            'Full_Name', 'Employee_Number', 'Role', 'Vehicle_Status',
+                            'Azuga_Device','Blackout' , 'Covered', 
                             'UL mileage','Azuga Mileage', 'Miles',
                             'Tech Miles','Idle Miles','Idle Minutes', 
                             'Idle %', 'MPG', 'PPG',
@@ -42,11 +45,14 @@ report = pd.DataFrame(columns = ['Branch', 'Area', 'Region',
                             'Derive eligible?', 'Derive completed', 'Tech Activity'])
 
 
-Branch_Area = pd.read_sql("""SELECT Branch_ID,
+if mode != "AtHome":
+    Branch_Area = pd.read_sql("""SELECT Branch_ID,
                            Region,
                            ActualRegion
                             FROM  rehab.A_BranchOffices_rtbl
                            """, connect())
+else:
+    Branch_Area = pd.read_csv("branch.csv")
 
 
 
@@ -69,13 +75,39 @@ Branch_Area.to_csv("branch.csv", index = False)
 
 report['Lease_Id'] = UL_PowerBi['Lease Id']
 report['Year'] = UL_PowerBi['Year']
-report['Make'] = UL_PowerBi['Make']
+report['Make'] = UL_PowerBi['Make'].str.lower()
 report['Model'] = UL_PowerBi['Model']
 report['VIN'] = UL_PowerBi['VIN']
 report['Plate_Number'] = UL_PowerBi['Plate Number']
 report['Full_Name'] = UL_PowerBi['Full Name']
 report['Employee_Number'] = UL_PowerBi['Employee Id']
+report['Vehicle_Status'] = UL_PowerBi['Vehicle Status']
 
+
+Car_Brands = ['honda',
+'chevrolet',
+'ford',
+'mercedes-Benz',
+'jeep',
+'bmw',
+'porsche',
+'subaru',
+'nissan',
+'cadillac',
+'volkswagen',
+'lexus',
+'ram',
+'buick',
+'toyota',
+'dodge',
+'gmc',
+'chrysler'
+]
+
+#Cleaning
+report = report[(report['Make']).isin(Car_Brands)]
+report = report[(report['Vehicle_Status'] != "Active") &
+                (report['Vehicle_Status'] != "Service Only")]
 
 report = sqldf("""
                   SELECT 
@@ -87,9 +119,12 @@ report = sqldf("""
                   report.Make,
                   report.Model,
                   report.VIN,
+                  report.Azuga_Device,
                   report.Plate_Number,
                   report.Full_Name,
-                  report.Employee_Number
+                  report.Employee_Number,
+                  report.Vehicle_Status,
+                  report.Blackout
                   FROM Branch_Area
                   LEFT JOIN report
                   ON  Branch_Area.Branch_ID = report.Branch 
@@ -98,16 +133,19 @@ report = sqldf("""
 #Getting Role from page1 Table
 role = pd.merge(report.astype(str), page1.astype(str), how = 'inner', left_on = "Employee_Number", right_on = "Employee Number" )
 report['Role'] = role["Job Title"]
-
 #Getting Azuga Device
+
 Azuga_device = pd.merge(report.astype(str), Vehicle_Report.astype(str), how = 'inner', on = "VIN" )
-Azuga_device['Device Serial Number'] = Azuga_device['Device Serial Number'].str.replace("s/\.\d*//;", "", regex = True) #Get rid of everything after .
-report['Azuga Device'] = Azuga_device["Device Serial Number"]
 
+Azuga_device['Device Serial Number'] = Azuga_device['Device Serial Number'].astype(str).apply(lambda x: x.replace('.0',''))
+print(Azuga_device['Device Serial Number'])
 
-# blackout_df = pd.merge(report.astype(str), blackout.astype(str), how = 'inner', left_on = "Azuga Device" ,right_on = "Device S/N")
-# print(blackout_df)
+report["Azuga_device"] = Azuga_device['Device Serial Number']
 
-# report['Black Out'] = blackout_df["Blackout since"]
+blackout_df = pd.merge(report.astype(
+    str), blackout.astype(str), how='inner', left_on = "Azuga_device", right_on = "Device S/N")
 
-report.to_csv("test.csv", index = False)
+report["Blackout"] = blackout_df["Blackout since"]
+print(blackout_df)
+
+report.to_csv("test.csv")
